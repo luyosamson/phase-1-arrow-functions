ed0e1a587966845690102317e2ee88e18fcd2d38
luyosamson <luyosamson@gmail.com>
2022-08-19 14:28:45 +0300

    Assignment completed

---
+++ index.js
@@ -0,0 +1,11 @@
+const divide=()=>2000/100
+
+//Arrow function square taking one parameter
+
+const square=x =>(x = x*2)
+square(2)
+
+//Arrow function add taking two parameters
+
+const add=(a,b)=>{return a+b}
+add(3,4)
